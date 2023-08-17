c71388624de46ace0fe23096bf46b5e587cd7f89
student <student@gnu.ac.in>
2023-08-17 12:28:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad4_63_/MainSignuupActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mad4_63_
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainSignuupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_signuup)
+    }
+}
