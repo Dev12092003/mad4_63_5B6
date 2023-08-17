f66ee0846258fee7482fa28b195acc57040205e7
Dev <dev.12092003@gmail.com>
2023-08-17 23:57:45 +0530

    commit

--- app/src/main/java/com/example/mad4_63_/MainSignuupActivity.kt
+++ app/src/main/java/com/example/mad4_63_/MainSignuupActivity.kt
@@ -1,11 +1,19 @@
 package com.example.mad4_63_
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainSignuupActivity : AppCompatActivity() {
+    lateinit var loginbtn:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_signuup)
+        loginbtn=findViewById(R.id.login_btn_e)
+        loginbtn.setOnClickListener{
+            val intent=Intent(this,MainLoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
